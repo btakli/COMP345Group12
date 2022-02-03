aaf56118eb002360289ad6c32227c3e182bd1c71
JChekkouri <74545845+JChekkouri@users.noreply.github.com>
2022-02-02 20:14:02 -0500

    Create Order.cpp

---
+++ Order.cpp
@@ -0,0 +1,172 @@
+#pragma once
+#include "Orders.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/**
+ * OrdersList implementation
+ * 
+ */
+
+OrdersList::OrdersList() { //Constructor
+
+}
+
+OrdersList::~OrdersList() { //Desctructor
+}
+
+OrdersList& OrdersList::operator=(const OrdersList& ol){ //Assignment Operator
+
+}
+
+OrdersList::OrdersList(const OrdersList& ol){ //Copy constructor
+
+}
+
+void OrdersList::move(int Move, int Location) {
+
+}
+
+void OrdersList::remove(int Remove) {
+}
+
+/**
+ * Order implementation
+ * 
+ */
+
+Order::Order() { //Constructor
+
+}
+
+Order::~Order() { //Desctructor
+}
+
+Order& Order::operator=(const Order& o){ //Assignment Operator
+
+}
+
+Order::Order(const Order& o){ //Copy constructor
+
+}
+
+/**
+ * Deploy implementation
+ * 
+ */
+
+Deploy::Deploy() { //Constructor
+
+}
+
+Deploy::~Deploy() { //Desctructor
+}
+
+Deploy& Deploy::operator=(const Deploy& dep){ //Assignment Operator
+
+}
+
+Deploy::Deploy(const Deploy& dep){ //Copy constructor
+
+}
+
+/**
+ * Advance implementation
+ * 
+ */
+
+Advance::Advance() { //Constructor
+
+}
+
+Advance::~Advance() { //Desctructor
+}
+
+Advance& Advance::operator=(const Advance& adv){ //Assignment Operator
+
+}
+
+Advance::Advance(const Advance& adv){ //Copy constructor
+
+}
+
+/**
+ * Bomb implementation
+ * 
+ */
+
+Bomb::Bomb() { //Constructor
+
+}
+
+Bomb::~Bomb() { //Desctructor
+}
+
+Bomb& Bomb::operator=(const Bomb& bmb){ //Assignment Operator
+
+}
+
+Bomb::Bomb(const Bomb& bmb){ //Copy constructor
+
+}
+
+/**
+ * Blockade implementation
+ * 
+ */
+
+Blockade::Blockade() { //Constructor
+
+}
+
+Blockade::~Blockade() { //Desctructor
+}
+
+Blockade& Blockade::operator=(const Blockade& blck){ //Assignment Operator
+
+}
+
+Blockade::Blockade(const Blockade& blck){ //Copy constructor
+
+}
+
+/**
+ * Airlift implementation
+ * 
+ */
+
+Airlift::Airlift() { //Constructor
+
+}
+
+Airlift::~Airlift() { //Desctructor
+}
+
+Airlift& Airlift::operator=(const Airlift& al){ //Assignment Operator
+
+}
+
+Airlift::Airlift(const Airlift& al){ //Copy constructor
+
+}
+
+/**
+ * Negotiate implementation
+ * 
+ */
+
+Negotiate::Negotiate() { //Constructor
+
+}
+
+Negotiate::~Negotiate() { //Desctructor
+}
+
+Negotiate& Negotiate::operator=(const Negotiate& ng){ //Assignment Operator
+
+}
+
+Negotiate::Negotiate(const Negotiate& ng){ //Copy constructor
+
+}
