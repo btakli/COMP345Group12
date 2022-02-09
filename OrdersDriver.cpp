c0f7923619422983a401fa00e2fd9e8ed7140854
JChekkouri <74545845+JChekkouri@users.noreply.github.com>
2022-02-09 00:38:16 -0500

    Update OrdersDriver.cpp

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -7,12 +7,33 @@ int main() {
 
     OrdersList orderslist; //create orders list 
 
-    //creeate each kind of order and place into the list
-    orderslist.addOrder(new Deploy());
-    orderslist.addOrder(new Advance());
-    orderslist.addOrder(new Bomb());
-    orderslist.addOrder(new Blockade());
-    orderslist.addOrder(new Airlift());
-	  orderslist.addOrder(new Negotiate());
+    //create each kind of order and place into the list
+    Order* o1 = new Deploy();
+    Order* o2 = new Advance();
+    Order* o3 = new Bomb();
+    Order* o4 = new Blockade();
+    Order* o5 = new Airlift();
+    Order* o6 = new Negotiate();
+
+    orderslist.addOrder(o1);
+    orderslist.addOrder(o2);
+    orderslist.addOrder(o3);
+    orderslist.addOrder(o4);
+    orderslist.addOrder(o5);
+    orderslist.addOrder(o6);
+	
+    delete(o1);
+    delete(o2);
+    delete(o3);
+    delete(o4);
+    delete(o5);
+    delete(o6);
+    
+    o1 = NULL;
+    o2 = NULL;
+    o3 = NULL;
+    o4 = NULL;
+    o5 = NULL;
+    o6 = NULL;
 
 };
