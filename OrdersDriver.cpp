4284854ca47cbac3a8d4faf46c3af2610bf19b59
JChekkouri <74545845+JChekkouri@users.noreply.github.com>
2022-02-13 15:02:13 -0500

    Update OrdersDriver.cpp

--- OrdersDriver.cpp
+++ OrdersDriver.cpp
@@ -1,39 +1,85 @@
 #include "Orders.h"
 
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
 using namespace std;
 
 int main() {
 
-    OrdersList orderslist; //create orders list 
+    //Create a new OrdersList
+    OrdersList* orderslist = new OrdersList();
 
-    //create each kind of order and place into the list
-    Order* o1 = new Deploy();
-    Order* o2 = new Advance();
-    Order* o3 = new Bomb();
-    Order* o4 = new Blockade();
-    Order* o5 = new Airlift();
-    Order* o6 = new Negotiate();
+    //Create the different kinds of orders
+    Order* o1 = new Order();
+    Order* o2 = new Deploy();
+    Order* o3 = new Advance();
+    Order* o4 = new Bomb();
+    Order* o5 = new Blockade();
+    Order* o6 = new Airlift();
+    Order* o7 = new Negotiate();
 
-    orderslist.addOrder(o1);
-    orderslist.addOrder(o2);
-    orderslist.addOrder(o3);
-    orderslist.addOrder(o4);
-    orderslist.addOrder(o5);
-    orderslist.addOrder(o6);
-	
-    delete(o1);
-    delete(o2);
-    delete(o3);
-    delete(o4);
-    delete(o5);
-    delete(o6);
+    //Put the orders in the first OrdersList using the addOrder() method
+    orderslist->addOrder(o1);
+    orderslist->addOrder(o2);
+    orderslist->addOrder(o3);
+    orderslist->addOrder(o4);
+    orderslist->addOrder(o5);
+    orderslist->addOrder(o6);
+    orderslist->addOrder(o7);
+
+
+    //print the OrdersList
+    cout<<"Orders in the list:"<< endl;
+    for (int i=0; i < orderslist->size(); i++) {
+        cout <<*orderslist->getOrder(i)<< endl;
+    }
+    cout <<"\n"<< endl;
     
+    //executing valid orders
+    cout <<"Executing orders if they are valid:"<< endl;
+    o1->execute(); //invalid order
+    o2->execute(); //valid order, prints its description.
+    cout <<"\n"<< endl;
+
+    //moving orders inside the list 
+    cout << "Before moving the orders in the list:" <<endl;
+    for (int i=0; i < orderslist->size(); i++) {
+       cout <<*orderslist->getOrder(i)<< endl;
+    }
+    orderslist->move(2,3);
+    cout <<"\n"<< endl;
+    cout << "After moving the orders in the list:" <<endl;
+    for (int i=0; i < orderslist->size(); i++) {
+       cout <<*orderslist->getOrder(i)<< endl;
+    }
+
+   // removing an order from the list
+   cout <<"\n"<< endl;
+    cout << "List before removing order number 3 in the list:" <<endl;
+	for (int i=0; i < orderslist->size(); i++) {
+        cout <<*orderslist->getOrder(i)<< endl;
+    }
+	orderslist->remove(3);
+    cout <<"\n"<< endl;
+	cout << "List after removing the order:" <<endl;
+	for (int i=0; i < orderslist->size(); i++) {
+        cout <<*orderslist->getOrder(i)<< endl;
+    }
+
+
+    //Delete everything
+    delete orderslist;
     o1 = NULL;
     o2 = NULL;
-    o3 = NULL;
-    o4 = NULL;
-    o5 = NULL;
-    o6 = NULL;
-
-};
+	o3 = NULL;
+	o4 = NULL;
+	o5 = NULL;
+	o6 = NULL;
+    o7 = NULL;
+    orderslist = NULL;
+    return 0;
+}
