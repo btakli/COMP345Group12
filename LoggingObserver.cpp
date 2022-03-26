e544b097620662fc323d6312d48581ed2170a3de
btakli <64867481+btakli@users.noreply.github.com>
2022-03-26 13:38:10 -0400

    Added preprocessor directive since time showing does NOT work on mac

--- LoggingObserver.cpp
+++ LoggingObserver.cpp
@@ -68,13 +68,15 @@ LogObserver& LogObserver::operator=(const LogObserver& rhs)
 
 void LogObserver::printGameStartTime()
 {
-	//Get time
+#ifndef __APPLE__
+	//Get time (DOES NOT WORK ON MACS, SO USING PREPROCESSOR DIRECTIVE)
 	auto start = std::chrono::system_clock::now();
 	auto legacyStart = std::chrono::system_clock::to_time_t(start);
 	char tmBuff[30];
 	ctime_s(tmBuff, sizeof(tmBuff), &legacyStart);
 
 	*_stream << "~~~~ Gamelog for game starting on: " << tmBuff << std::endl;
+#endif
 }
 
 void LogObserver::update(ILoggable* obj) {
